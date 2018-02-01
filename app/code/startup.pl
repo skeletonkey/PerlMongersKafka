027f63ba42c87587afdb3549bf1ff127d8f0b5aa
Erik Tank <erik@jundy.com>
2018-02-01 00:35:08 -0700

    Basic producer and consumer

---
+++ app/code/startup.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+print "I'm going to docker exec into this container to play.
+ For it to exist and be on the docker-compose network
+ it needs to do something - else it'll simply exit.\n";
+
+sleep 86400;
+
